38e2cccf30e038a6ee18a56b96d57083243126c1
yanessa18 <yanasirichenko0@gmail.com>
2024-10-14 12:11:11 +0300

    SirichenkoLW1

--- ЛР 1/SirichenkoLW1.sql
+++ ЛР 1/SirichenkoLW1.sql
@@ -218,6 +218,20 @@ go
 
 
 
+SELECT *
+FROM [dbo].[CUSTOMERS];
+
+SELECT *
+FROM [dbo].[OFFICES];
+
+SELECT *
+FROM [dbo].[ORDERS];
+
+SELECT *
+FROM [dbo].[PRODUCTS];
+
+SELECT *
+FROM [dbo].[SALESREPS];
 
 
 
