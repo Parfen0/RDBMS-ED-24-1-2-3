780353de60fe12c5e124d5a30ee3781863ac4ec0
LZAnastasia <lozova_a@365.dnu.edu.ua>
2024-11-04 17:37:24 +0200

    LozovaLW5
    LozovaLW5

---
+++ ЛР 5/LozovaLW5.sql
@@ -0,0 +1,43 @@
+﻿USE WebStor;
+
+-- Завдання 1 --
+
+SELECT
+    EMPL_NUM, 
+	NAME, 
+	AGE,
+	TITLE 
+
+FROM
+    [dbo].[SALESREPS]
+
+WHERE 
+    Age > 45
+
+ORDER BY 
+    Age DESC;
+
+-- Завдання 2 --
+
+SELECT DISTINCT MFR, 
+                PRODUCT 
+				 
+FROM 
+    [dbo].[ORDERS] 
+
+WHERE YEAR(ORDER_DATE) = 2008;
+
+--Завдання 3 --
+
+SELECT TOP 1 WITH TIES
+    REP AS EmployeeID,
+	COUNT(*) AS OrderCount 
+
+FROM 
+    [dbo].[ORDERS]
+
+WHERE YEAR(ORDER_DATE) = 2008
+
+GROUP BY REP
+
+ORDER  BY COUNT(*) DESC;
