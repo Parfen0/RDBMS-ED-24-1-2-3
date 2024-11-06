4906a2150d48b8b7c81c6589df58bf892b054969
gorbenkoviktoria <viktoriagorbenko52@gmail.com>
2024-11-06 23:26:44 +0200

    HorbenkoLW5
    HorbenkoLW5

---
+++ ЛР 5/HorbenkoLW5.sql
@@ -0,0 +1,27 @@
+﻿USE WebStor
+--1 завдання--
+SELECT
+      EMPL_NUM,
+       NAME, 
+       AGE, 
+       TITLE
+ FROM [dbo]. [SALESREPS]
+ WHERE Age > 45
+ -- 2 завдання--
+SELECT DISTINCT MFR,
+PRODUCT
+FROM
+
+[dbo] . [ORDERS]
+ WHERE YEAR(ORDER_DATE) = 2008;
+
+--Завдання 3 --
+
+SELECT TOP (10) WITH TIES 
+    REP AS EmployeeID,
+COUNT (*) AS OrderCount
+FROM
+    [dbo] . [ORDERS]
+WHERE YEAR(ORDER_DATE) = 2008
+GROUP BY REP 
+ORDER BY COUNT(*) DESC; 
