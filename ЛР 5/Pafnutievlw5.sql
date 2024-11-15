350bf9ce1ee286d6d0793658359ee560f7aac6a3
BohdanSly00 <bohdanuniversitet@gmail.com>
2024-11-15 14:03:11 +0100

    Pafnutievlw5
    Pafnutievlw5

---
+++ ЛР 5/Pafnutievlw5.sql
@@ -0,0 +1,42 @@
+USE WebStor1;
+
+
+SELECT 
+    EMPL_NUM,      
+    NAME,       
+    AGE,            
+    TITLE           
+FROM 
+    [dbo].[SALESREPS]
+WHERE 
+    AGE > 45
+ORDER BY 
+    AGE DESC;
+
+
+	SELECT column_name, data_type 
+FROM information_schema.columns 
+WHERE table_name = 'ORDERS';
+
+
+SELECT DISTINCT 
+    MFR AS manufacturer_id, 
+    PRODUCT AS product_id
+FROM 
+    [dbo].[ORDERS]
+WHERE 
+    YEAR(ORDER_DATE) = 2008;
+
+
+
+	SELECT TOP 1 WITH TIES 
+    REP AS employee_id, 
+    COUNT(*) AS order_count
+FROM 
+    [dbo].[ORDERS]
+WHERE 
+    YEAR(ORDER_DATE) = 2008
+GROUP BY 
+    REP
+ORDER BY 
+    COUNT(*) DESC;
