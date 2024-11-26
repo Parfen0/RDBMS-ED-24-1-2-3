0630b1896b3f1b2d3574ead10df9b2898a1303b3
warumiss <uwarumiss@gmail.com>
2024-11-26 16:46:57 +0200

    BocharovaLW6
    BocharovaLW6

---
+++ ЛР 6/BocharovaLW6.sql
@@ -0,0 +1,64 @@
+﻿﻿USE WebStor;
+
+/* Завдання 1 */
+SELECT Office, City, MGR
+FROM [dbo].[OFFICES]
+WHERE Region = 'Eastern' AND 
+      TARGET <= 350000.00; 
+
+/* Завдання 2.1 */
+SELECT ORDER_NUM, ORDER_DATE, CUST, REP, MFR, PRODUCT, QTY, AMOUNT 
+FROM [dbo].[ORDERS]
+WHERE YEAR(ORDER_DATE) NOT BETWEEN 2008 AND 2008
+    AND (Product LIKE '_A%' OR Product LIKE '%0%');
+
+/* Завдання 2.2 */
+SELECT MFR, 
+    COUNT(DISTINCT ORDER_NUM) AS UniqueOrders,
+    SUM(Amount) AS TotalAmount
+FROM [dbo].[ORDERS]
+WHERE YEAR(ORDER_DATE) NOT BETWEEN 2008 AND 2008
+    AND (Product LIKE '_A%' OR Product LIKE '%0%')
+GROUP BY MFR
+ORDER BY TotalAmount ASC;
+
+/* Завдання 2.3 */
+SELECT TOP 1 WITH TIES MFR, 
+    SUM(Amount) AS TotalAmount
+FROM [dbo].[ORDERS]
+WHERE YEAR(ORDER_DATE) NOT BETWEEN 2008 AND 2008
+    AND (Product LIKE '_A%' OR Product LIKE '%0%')
+GROUP BY MFR
+ORDER BY TotalAmount DESC;
+
+/* Завдання 3 */
+SELECT TOP 1 WITH TIES REP_OFFICE, 
+    COUNT(*) AS EmployeeCount
+FROM [dbo].[SALESREPS]
+WHERE TITLE = 'Sales Rep' AND 
+    Age IN (29, 45, 48)
+GROUP BY REP_OFFICE
+ORDER BY EmployeeCount DESC;
+
+/* Завдання 4 */
+SELECT TOP 1 WITH TIES MFR_ID, Product_ID, [Description], 
+    LEN([Description]) AS DescriptionLength
+FROM [dbo].[PRODUCTS]
+ORDER BY DescriptionLength DESC;
+
+/* Завдання 5 */
+SELECT DISTINCT 
+    CONCAT_WS(' ', RTRIM(MFR_ID), RTRIM(PRODUCT_ID), RTRIM([Description])) AS ConcatenatedString,
+    LEN(RTRIM(CONCAT_WS(' ', MFR_ID, PRODUCT_ID, [Description]))) AS StringLength
+FROM [dbo].[PRODUCTS]
+WHERE PRODUCT_ID NOT LIKE '%[^0-9]%'
+ORDER BY StringLength DESC;
+
+/* Завдання 6 */
+SELECT TOP 1 WITH TIES CUST_NUM, 
+    CONCAT(LEFT(COMPANY, 2), 
+    REPLICATE('*', LEN(COMPANY) - 4), 
+    RIGHT(COMPANY, 2)) AS MaskedName,
+    CREDIT_LIMIT
+FROM [dbo].[CUSTOMERS]
+ORDER BY CREDIT_LIMIT ASC;
